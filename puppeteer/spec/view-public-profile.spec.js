d214137004d16f5b4f32185674089dfc40504fcd
Gabriel Weyer <gabrielweyer@gmail.com>
2018-12-25 10:04:16 +1100

    Increase Jasmine timeout to get actual error

--- puppeteer/spec/view-public-profile.spec.js
+++ puppeteer/spec/view-public-profile.spec.js
@@ -3,16 +3,20 @@ const config = require('../config');
 
 describe('ViewPublicProfile', () => {
   describe('Given navigate to public profile', () => {
+    beforeEach(() => {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+    });
+
     it('Then display full name', async () => {
-      const fullNameSelector = '.vcard-fullname';
+      const fullnameSelector = '.vcard-fullname';
 
       const browser = await puppeteer.launch({headless: true});
       const page = await browser.newPage();
 
       await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
 
-      await page.waitForSelector(fullNameSelector, { visible: true });
-      const actualFullname = await page.evaluate((selector) => document.querySelector(selector).innerText, fullNameSelector);
+      await page.waitForSelector(fullnameSelector, { visible: true });
+      const actualFullname = await page.evaluate((selector) => document.querySelector(selector).innerText, fullnameSelector);
 
       expect(actualFullname).toBe(config.gitHub.publicProfile.expectedFullname);
 

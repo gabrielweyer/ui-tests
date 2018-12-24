f00e59747f4c01b408447611e15b4494f5b50dce
Gabriel Weyer <gabrielweyer@gmail.com>
2018-12-24 17:53:15 +1100

    Revert "Stop using fat arrow functions"
    This reverts commit e183d50768c67e470ef4f5b34e318cea1b3cb056.
    
    This commit didn't fix the issue.

--- puppeteer/spec/view-public-profile.spec.js
+++ puppeteer/spec/view-public-profile.spec.js
@@ -1,25 +1,25 @@
 const puppeteer = require('puppeteer');
 const config = require('../config');
 
-describe('ViewPublicProfile', function() {
+describe('ViewPublicProfile', () => {
   let browser;
   let page;
 
-  beforeAll(async function() {
+  beforeAll(async () => {
     browser = await puppeteer.launch({headless: true});
     page = await browser.newPage();
   });
 
-  afterAll(async function() {
+  afterAll(async () => {
     await browser.close();
   });
 
-  describe('Given navigate to public profile', function() {
-    beforeEach(async function() {
+  describe('Given navigate to public profile', () => {
+    beforeEach(async () => {
       await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
     });
 
-    it('Then display full name', async function() {
+    it('Then display full name', async () => {
       const fullNameSelector = '.vcard-fullname';
 
       await page.waitForSelector(fullNameSelector, { visible: true });

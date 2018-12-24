634042a8fb901f82ef6c7e1504d82908b4f0e7a2
Gabriel Weyer <gabrielweyer@gmail.com>
2018-12-25 09:08:11 +1100

    Put all code in test body

--- puppeteer/spec/view-public-profile.spec.js
+++ puppeteer/spec/view-public-profile.spec.js
@@ -1,33 +1,22 @@
 const puppeteer = require('puppeteer');
 const config = require('../config');
 
-
-
 describe('ViewPublicProfile', () => {
-  var browser;
-  var page;
-
-  beforeAll(async () => {
-    browser = await puppeteer.launch({headless: true});
-    page = await browser.newPage();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
   describe('Given navigate to public profile', () => {
-    beforeEach(async () => {
-      await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
-    });
-
     it('Then display full name', async () => {
       const fullNameSelector = '.vcard-fullname';
+
+      const browser = await puppeteer.launch({headless: true});
+      const page = await browser.newPage();
+
+      await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
 
       await page.waitForSelector(fullNameSelector, { visible: true });
       const actualFullname = await page.evaluate((selector) => document.querySelector(selector).innerText, fullNameSelector);
 
       expect(actualFullname).toBe(config.gitHub.publicProfile.expectedFullname);
+
+      await browser.close();
     });
   });
 });

b79e157dc43e231b4734a554ce171b3eb30c0389
Gabriel Weyer <gabrielweyer@gmail.com>
2018-12-25 10:21:58 +1100

    Move test back to initial structure

--- puppeteer/spec/view-public-profile.spec.js
+++ puppeteer/spec/view-public-profile.spec.js
@@ -4,21 +4,32 @@ const config = require('../config');
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
 describe('ViewPublicProfile', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({headless: true});
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    if (browser) {
+      await browser.close();
+    }
+  });
+
   describe('Given navigate to public profile', () => {
+    beforeEach(async () => {
+      await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
+    });
+
     it('Then display full name', async () => {
       const fullnameSelector = '.vcard-fullname';
-
-      const browser = await puppeteer.launch({headless: true});
-      const page = await browser.newPage();
-
-      await page.goto(`https://github.com/${config.gitHub.publicProfile.username}`);
 
       await page.waitForSelector(fullnameSelector, { visible: true });
       const actualFullname = await page.evaluate((selector) => document.querySelector(selector).innerText, fullnameSelector);
 
       expect(actualFullname).toBe(config.gitHub.publicProfile.expectedFullname);
-
-      await browser.close();
     });
   });
 });

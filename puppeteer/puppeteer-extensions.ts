40345d2b097123c07394eec2324426ada980272d
Gabriel Weyer <gabrielweyer@gmail.com>
2022-04-09 11:16:40 +1000

    Run Puppeteer tests using GitHub Actions

--- puppeteer/puppeteer-extensions.ts
+++ puppeteer/puppeteer-extensions.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 export async function saveScreenshot(page : puppeteer.Page, filenameNoPathNoExtension: string): Promise<void> {
   try {
-    fs.mkdirSync(config.screenshotsAbsolutePath)
+    fs.mkdirSync(config.screenshotsAbsolutePath);
   } catch (error) {
     if (error.code !== 'EEXIST') {
       console.warn('Could not create screenshots directory', config.screenshotsAbsolutePath, error);

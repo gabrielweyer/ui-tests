1fe1d3ecf2f81cebe03fa7876c8b6043180cc309
Gabriel Weyer <gabrielweyer@gmail.com>
2022-11-13 11:03:39 +1100

    Fix Nullable Reference Type warnings

--- selenium-csharp/src/SeleniumCSharp/WebDriverExtensions/ExpectedConditions.cs
+++ selenium-csharp/src/SeleniumCSharp/WebDriverExtensions/ExpectedConditions.cs
@@ -3,14 +3,14 @@ using OpenQA.Selenium;
 
 namespace SeleniumCSharp.WebDriverExtensions;
 
-public class ExpectedConditions
+public static class ExpectedConditions
 {
     /// <summary>
     /// An expectation for checking whether two or more elements are visible and enabled.
     /// </summary>
     /// <param name="locator">The locator used to find the elements.</param>
-    /// <returns>The <see cref="ReadOnlyCollection&lt;IWebElement&gt;"/> once they are located, visible and enabled.</returns>
-    public static Func<IWebDriver, ReadOnlyCollection<IWebElement>> ElementsAreEnabled(By locator)
+    /// <returns>The elements if they are located, visible and enabled. Null otherwise.</returns>
+    public static Func<IWebDriver, ReadOnlyCollection<IWebElement>?> ElementsAreEnabled(By locator)
     {
         return driver =>
         {
@@ -20,7 +20,9 @@ public class ExpectedConditions
             {
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
                 var elements = driver.FindElements(locator);
-                return (elements.Count > 1 && elements.All(e => e.Displayed && e.Enabled)) ? elements : null;
+                return elements.Count > 1 && elements.All(e => e.Displayed && e.Enabled) ?
+                    elements :
+                    null;
             }
             catch (Exception e) when (e is NoSuchElementException || e is StaleElementReferenceException)
             {

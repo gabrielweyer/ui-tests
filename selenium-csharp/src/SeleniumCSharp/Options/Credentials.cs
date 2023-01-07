58d7fc4048d1fce2ceab21e99c3fa50e335da2ba
Gabriel Weyer <gabrielweyer@gmail.com>
2023-01-07 16:25:56 +1100

    Use Meziantou analyzers

---
+++ selenium-csharp/src/SeleniumCSharp/Options/Credentials.cs
@@ -0,0 +1,7 @@
+namespace SeleniumCSharp.Options;
+
+public class Credentials
+{
+    public string EmailAddress { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
---
+++ selenium-csharp/src/SeleniumCSharp/Options/PublicProfile.cs
@@ -0,0 +1,7 @@
+namespace SeleniumCSharp.Options;
+
+public class PublicProfile
+{
+    public string Username { get; set; } = default!;
+    public string ExpectedFullname { get; set; } = default!;
+}
